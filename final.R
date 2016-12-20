b04e2b8c76e0cfda16a8cb9b002b3d93e9e94514
XiaoyueZhang <modifiednvince@gmail.com>
2016-12-20 11:25:30 -0500

    thx

---
+++ final.R
@@ -0,0 +1,7 @@
+### Group 3 - Yoyo performance
+#"xiao Ting, Jiaxi Li , Xiangyu Wang, Xiaoyue Zhang"
+#"Measuring Performance on Yoyo Playing for Expert versus Beginner"
+#"https://drive.google.com/open?id=0B9d688aiWRHnUUNNOXM1bFU3VGc"
+
+  
+#"If your like for further information or to look at the process of recording on playing yoyo,please email me at jl4537@tc.columbia.edu."

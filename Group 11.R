8d8e943e3b4ed36c770c56c588dfad1e7b0312cf
Beibei Cao <54962737+beibeicao28@users.noreply.github.com>
2019-12-10 20:42:41 -0500

    Group 11
    Done!

---
+++ Group 11.R
@@ -0,0 +1,9 @@
+---
+title: "Group 11 Submission Link"
+date: "December 10, 2019"
+output: html_document
+---
+  
+  ### Group 11 - Long Standing Jump
+  # Group member: Beibei Cao, Yaqi Lu, Yixiao Li, Zhaozhuo Zheng
+  # Link: https://www.vialogues.com/vialogues/play/54025/
